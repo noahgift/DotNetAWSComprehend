a651343a61165153fd8801a172b658efe8c44c8d
Noah Gift <noah.gift@gmail.com>
2022-01-07 20:27:59 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+using Amazon.Comprehend;
+using Amazon.Comprehend.Model;
+
+
+namespace Comprehend
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            // Display title
+            Console.WriteLine("AWS AI API Sentiment Detector" + Environment.NewLine);
+
+            // Ask for phrase
+            Console.WriteLine("What phrase would you like to Detect Sentiment From? " + Environment.NewLine);
+            var phrase = Console.ReadLine();
+            
+            // Detect Sentiment
+            AmazonComprehendClient comprehendClient = new AmazonComprehendClient();
+            Console.WriteLine("Calling DetectSentiment");
+            DetectSentimentRequest detectSentimentRequest = new DetectSentimentRequest()
+            {
+                Text = phrase,
+                LanguageCode = "en"
+            };
+
+            DetectSentimentResponse detectSentimentResponse = await
+            comprehendClient.DetectSentimentAsync(detectSentimentRequest);
+            Console.WriteLine(detectSentimentResponse.Sentiment);
+            Console.WriteLine("Done");
+        }
+    }
+}
